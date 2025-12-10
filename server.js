6e4eaac7ca7841e553edfd1e2218025962057c34
John Akpa Ojonugwa <johnakpaojonugwa@gmail.com>
2025-12-10 12:25:32 +0100

    edited the server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { connectDB } from './config/db.js';
 import teacherRoutes from './routes/teacher.routes.js';
 import studentRoutes from './routes/student.routes.js';
@@ -11,15 +12,23 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// const allowedOrigin = [process.env.CORSORIGIN];
+// app.use(cors({
+//     origin: allowedOrigin,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }))
+
+app.use(cors())
+
 connectDB();
 
 //Routes
-app.use('/api/v1/teachers', teacherRoutes); //http://localhost:4777/api/v1/teachers/register
+app.use('/api/v1/teachers', teacherRoutes); 
 // student routes
 app.use('/api/v1/students', studentRoutes);
 // course routes
 app.use('/api/v1/courses', courseRoutes);
-const PORT = process.env.PORT_NO || 4777;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port http://localhost:${PORT}`);
 })
